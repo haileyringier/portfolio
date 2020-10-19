8e0bef5155525efedbd9b343008ecaf2196a860b
haileyringier <haileyringier@outlook.com>
2020-10-19 11:58:08 -0600

    replaces paper with cards

--- portfolio/src/Components/Projects.js
+++ portfolio/src/Components/Projects.js
@@ -2,6 +2,14 @@ import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -11,7 +19,7 @@ const useStyles = makeStyles((theme) => ({
       padding: theme.spacing(2),
       textAlign: 'center',
       color: theme.palette.text.secondary,
-      height: '100%',
+    //   height: '100%',
     },
   }));
 
@@ -20,37 +28,74 @@ export default function Projects(){
     const classes = useStyles();
 
     return(
-        <div className='project-section'>
-            <p>My Projects</p>
-        <Grid container spacing={4}>
-            <Grid item xs={4}>
-                <Paper className={classes.paper}>
-                    <h3>Flappy Bird</h3>
-                    <ul>
-                        <li>React</li>
-                        <li>HTML</li>
-                        <li>classes</li>
-                    </ul>
-                    <img></img>
-                </Paper>
-            </Grid>
-            <Grid item xs={4}>
-                <Paper className={classes.paper}>
-                    <h3>Productivity Journal</h3>
-                    <ul>
-                        <li>React</li>
-                        <li>Material-UI</li>
-                        <li>Chart.js</li>
-                        <li>Node</li>
-                        <li>Express</li>
-                    </ul>
-                    <img></img>
-                </Paper>
-            </Grid>
-                
-
-        </Grid>
-        </div>
-        
+//         <div className='project-section'>
+//             <p>My Projects</p>
+//         <Grid container spacing={4}>
+//             
+//             </Grid>
+//             <Grid item xs={4}>
+//                 
+//             </Grid>
+//         </Grid>
+//         </div>
+//     )
+// }
+<Grid>
+<Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          alt="Productivity Journal dashboard"
+          height="140"
+          image="../Pictures/Dashboard.png"
+          title="Productivity Journal"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            Flappy Bird
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            Remake of the popular game Flappy Bird using React.
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Github
+        </Button>
+        <Button size="small" color="primary">
+          Demo
+        </Button>
+      </CardActions>
+    </Card>
+    <Card className={classes.root}>
+    <CardActionArea>
+      <CardMedia
+        component="img"
+        alt="Productivity Journal dashboard"
+        height="140"
+        image="/Users/haileyringier/Projects/portfolio/portfolio/Pictures/Dashboard.png"
+        title="Productivity Journal"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="h2">
+          Productivity Journal
+        </Typography>
+        <Typography variant="body2" color="textSecondary" component="p">
+          Final Project with Flatiron created using React, Material-UI, 
+          Node and Express.
+        </Typography>
+      </CardContent>
+    </CardActionArea>
+    <CardActions>
+      <Button size="small" color="primary">
+        Github
+      </Button>
+      <Button size="small" color="primary">
+        Demo
+      </Button>
+    </CardActions>
+  </Card>
+  </Grid>
     )
 }
